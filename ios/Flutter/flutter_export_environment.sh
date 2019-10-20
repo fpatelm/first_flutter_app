9649c0af0f72e49bfc7458b602e2e6704bb7c1f2
Faizal Patel <faizalmw@hotmail.com>
2019-10-20 14:51:19 +0100

    Part 1 InfiniteListView

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/fpatel/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/fpatel/Projects/tutorial"
-export "FLUTTER_TARGET=/Users/fpatel/Projects/tutorial/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/fpatel/Projects/first_flutter_app"
+export "FLUTTER_TARGET=/Users/fpatel/Projects/first_flutter_app/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/fpatel/flutter/bin/cache/artifacts/engine/ios"
--- lib/main.dart
+++ lib/main.dart
@@ -23,16 +23,54 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 class RandomWords extends StatefulWidget {
   RandomWords({Key key}) : super(key: key);
-  _RandomWordsState createState() => _RandomWordsState();
+  RandomWordsState createState() => RandomWordsState();
 }
 
-class _RandomWordsState extends State<RandomWords> {
+class RandomWordsState extends State<RandomWords> {
   final WordPair wordPair = WordPair.random();
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = TextStyle(fontSize: 20);
+
   @override
   Widget build(BuildContext context) {
-    return Text(wordPair.asPascalCase),tyle: TextStyle(fontSize: 30);
+    return _buildSuggestions();
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (BuildContext _context, int i) {
+        // Add a one-pixel-high divider widget before each row
+        // in the ListView.
+        if (i.isOdd) {
+          return Divider();
+        }
+        // The syntax "i ~/ 2" divides i by 2 and returns an
+        // integer result.
+        // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+        // This calculates the actual number of word pairings
+        // in the ListView,minus the divider widgets.
+        final int index = i ~/ 2;
+        // If you've reached the end of the available word
+        // pairings...
+        if (index >= _suggestions.length) {
+          // ...then generate 10 more and add them to the
+          // suggestions list.
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
